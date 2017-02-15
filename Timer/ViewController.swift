7576eeb089f044e8634026bb2361929bd78e6568
Paul Baranay <pbaranay@gmail.com>
2017-02-14 22:42:55 -0500

    Fix start/pause/stop buttons

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -13,12 +13,15 @@ let start_time = 45
 class ViewController: UIViewController {
 
     @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startLabel: UIButton!
+    @IBOutlet weak var pauseLabel: UIButton!
+    @IBOutlet weak var stopLabel: UIButton!
     
     var seconds = start_time
     var timer = Timer()
     var timerIsOn = false
     
-    @IBAction func startButton(_ sender: Any) {
+    func startButton() {
         
         if timerIsOn == false {
             timer = Timer.scheduledTimer(timeInterval: 1,
@@ -27,7 +30,7 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func stopButton(_ sender: Any) {
+    func stopButton() {
         
         timer.invalidate()
         seconds = start_time
@@ -35,12 +38,25 @@ class ViewController: UIViewController {
         timerIsOn = false
     }
     
-    @IBAction func pauseButton(_ sender: Any) {
+    func pauseButton() {
     
         timer.invalidate()
         timerIsOn = false
     }
     
+    @IBAction func handleTimer(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            startButton()
+        case 2:
+            pauseButton()
+        case 3:
+            stopButton()
+        default:
+            print("error")
+        }
+    }
+
     func updateTimer() {
         
         seconds -= 1
