bfd4ee16b53d4b006fa64967908c0d3061ae817f
Paul Baranay <pbaranay@gmail.com>
2017-02-14 22:13:24 -0500

    Initial commit: Timer counts down, pause button same as stop

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -8,8 +8,46 @@
 
 import UIKit
 
+let start_time = 45
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var seconds = start_time
+    var timer = Timer()
+    var timerIsOn = false
+    
+    @IBAction func startButton(_ sender: Any) {
+        
+        if timerIsOn == false {
+            timer = Timer.scheduledTimer(timeInterval: 1,
+                                         target: self, selector: ("updateTimer"), userInfo: nil, repeats: true)
+            timerIsOn = true
+        }
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        
+        timer.invalidate()
+        seconds = start_time
+        timerLabel.text = "\(seconds)"
+        timerIsOn = false
+    }
+    
+    @IBAction func pauseButton(_ sender: Any) {
+    
+        timer.invalidate()
+        timerIsOn = false
+    }
+    
+    func updateTimer() {
+        
+        seconds -= 1
+        timerLabel.text = "\(seconds)"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
